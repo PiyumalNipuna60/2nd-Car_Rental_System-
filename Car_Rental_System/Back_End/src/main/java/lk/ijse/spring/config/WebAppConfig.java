924558e76c122ce2dd53cbc4be1aaead445fcadc
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-19 22:20:34 +0530

    Add Content

---
+++ Car_Rental_System/Back_End/src/main/java/lk/ijse/spring/config/WebAppConfig.java
@@ -0,0 +1,13 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan("lk.ijse.spring")
+@EnableWebMvc
+public class WebAppConfig {
+
+
+}
---
+++ Car_Rental_System/Back_End/src/main/java/lk/ijse/spring/config/WebRootConfig.java
@@ -0,0 +1,9 @@
+package lk.ijse.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+//@Import()
+public class WebRootConfig {
+}
