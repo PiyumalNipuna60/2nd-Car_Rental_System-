87065a64a25a2abaf0eda1b5008790324fb20da4
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-19 22:26:49 +0530

    Add AppWideExceptionHandler class

---
+++ Car_Rental_System/Back_End/src/main/java/lk/ijse/spring/advisor/AppWideExceptionHandler.java
@@ -0,0 +1,18 @@
+package lk.ijse.spring.advisor;
+
+import lk.ijse.spring.util.ResponseUtil;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class AppWideExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({Exception.class})
+    public ResponseUtil exceptionHandler(Exception e){
+        return new ResponseUtil(500,e.getMessage(),null);
+    }
+
+}
