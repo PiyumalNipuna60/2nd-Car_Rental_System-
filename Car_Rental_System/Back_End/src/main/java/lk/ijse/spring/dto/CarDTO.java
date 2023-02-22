e842cb705f19dd5895e2d9f7899fdb3b87388156
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-22 22:52:40 +0530

    Add CarDTO class

---
+++ Car_Rental_System/Back_End/src/main/java/lk/ijse/spring/dto/CarDTO.java
@@ -0,0 +1,32 @@
+package lk.ijse.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class CarDTO {
+    private String registrationNO;
+    private String brand;
+    private String type;
+    private int noOfPassengers;
+    private String transmissionType;
+    private String fuelType;
+    private String color;
+    private byte[] frontView;
+    private byte[] backView;
+    private byte[] sideView;
+    private byte[] internalView;
+    private double dailyRate;
+    private double monthlyRate;
+    private double freeKmForPrice;
+    private double freeKmForDuration;
+    private double lossDamageWaiver;
+    private double priceForExtraKm;
+    private double completeKm;
+    private final String status="Available";
+}
