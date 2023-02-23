ffa84ea671e4b57d791337d07fcc2ed99e476f80
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-23 23:18:00 +0530

    Add CustomerDTO class

---
+++ Car_Rental_System/Back_End/src/main/java/lk/ijse/spring/dto/CustomerDTO.java
@@ -0,0 +1,26 @@
+package lk.ijse.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class CustomerDTO {
+    private String customerId;
+    private String name;
+    private String address;
+    private int contactNo;
+    private String email;
+    private String nicNo;
+    private String nicFrontImg;
+    private String nicBackImg;
+    private String licenceNo;
+    private String licenceImg;
+    private String username;
+    private String password;
+    private final String status = "Pending";
+}
