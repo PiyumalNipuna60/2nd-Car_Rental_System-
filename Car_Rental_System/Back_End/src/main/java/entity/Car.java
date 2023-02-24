c5db9e0392920f1ae51296dd65fbe2c5a68d8cc8
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-24 20:01:34 +0530

    add Car class

---
+++ Car_Rental_System/Back_End/src/main/java/entity/Car.java
@@ -0,0 +1,45 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
+public class Car {
+    @Id
+    private String registrationNO;
+    private String brand;
+    private String type;
+    private int noOfPassengers;
+    private String transmissionType;
+    private String fuelType;
+    private String color;
+    private byte[] frontView;
+    private byte[] backView;
+    private byte[] sideView;
+    private byte[] internalView;
+    private double dailyRate;
+    private double monthlyRate;
+    private double freeKmForPrice;
+    private double freeKmForDuration;
+    private double lossDamageWaiver;
+    private double priceForExtraKm;
+    private double completeKm;
+    private final String status="Available";
+
+@OneToMany(mappedBy = "car",cascade = CascadeType.ALL)
+    private List<CarRent> rentals =new ArrayList();
+}
