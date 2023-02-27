04fe1b5f41bc651fdc6312074f3505589548c9d4
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-02-27 19:35:29 +0530

    Add Service  class

---
+++ Car_Rental_System/Back_End/src/main/java/lk/ijse/spring/service/PaymentService.java
@@ -0,0 +1,24 @@
+package lk.ijse.spring.service;
+
+import lk.ijse.spring.dto.PaymentDTO;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface PaymentService {
+    void savePayment(PaymentDTO dto);
+
+    void updatePayment(PaymentDTO dto);
+
+    void deletePayment(String paymentId);
+
+    PaymentDTO searchPayment(String paymentId);
+
+    List<PaymentDTO> getAllPayments();
+
+    List<PaymentDTO> getAllPaymentsByDateRange(LocalDate fromDate, LocalDate toDate);
+
+    List<PaymentDTO> getAllPaymentsByCustomerId(String customerId);
+
+    String generatePaymentId();
+}
