bd020a1882cc3500f6ec1f41ff4760da2e257378
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-03-01 23:59:38 +0530

    Update content

--- Car_Rental_System/Back_End/src/main/java/lk/ijse/spring/advisor/AppWideExceptionHandler.java
+++ Car_Rental_System/Back_End/src/main/java/lk/ijse/spring/advisor/AppWideExceptionHandler.java
@@ -2,17 +2,18 @@ package lk.ijse.spring.advisor;
 
 import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.annotation.RestController;
 
-@RestControllerAdvice
+@RestController
+@CrossOrigin
 public class AppWideExceptionHandler {
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({Exception.class})
-    public ResponseUtil exceptionHandler(Exception e){
-        return new ResponseUtil(500,e.getMessage(),null);
+        @ResponseStatus(HttpStatus.BAD_REQUEST)
+        @ExceptionHandler({Exception.class})
+        public ResponseUtil exceptionHandler(Exception e){
+            return new ResponseUtil(500,e.getMessage(),null);
+        }
     }
 
-}
